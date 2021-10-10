be4fce9d3372d9ae147f17398db588e8371e0c27
dkxce <dkxce@mail.ru>
2021-10-10 17:35:31 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyAPRSCodeGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = CallsignChecksum(textBox1.Text.Trim()).ToString();
+        }
+
+        public static int CallsignChecksum(string callsign)
+        {
+            int stophere = callsign.IndexOf("-");
+            if (stophere > 0) callsign = callsign.Substring(0, stophere);
+            string realcall = callsign.ToUpper();
+            while (realcall.Length < 10) realcall += " ";
+
+            // initialize hash 
+            int hash = 0x73e2;
+            int i = 0;
+            int len = realcall.Length;
+
+            // hash callsign two bytes at a time 
+            while (i < len)
+            {
+                hash ^= (int)(realcall.Substring(i, 1))[0] << 8;
+                hash ^= (int)(realcall.Substring(i + 1, 1))[0];
+                i += 2;
+            }
+            // mask off the high bit so number is always positive 
+            return hash & 0x7fff;
+        }
+    }
+}
